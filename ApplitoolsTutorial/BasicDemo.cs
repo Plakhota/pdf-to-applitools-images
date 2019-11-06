45e44bcde1185b9921e258e1c395ce8bce112a8c
Itai Bar-Haim <itai.barhaim@applitools.com>
2019-11-06 16:32:42 +0200

    Initial commit.

---
+++ ApplitoolsTutorial/BasicDemo.cs
@@ -0,0 +1,55 @@
+﻿using Applitools.Images;
+using NUnit.Framework;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+
+namespace ApplitoolsTutorial
+{
+
+    [TestFixture]
+    public class BasicDemo
+    {
+        private Eyes eyes;
+
+        [Test]
+        public void BasicTest()
+        {
+            // Initialize the eyes SDK and set your private API key.
+            eyes = new Eyes();
+           
+            // Start the session and set app name and test name.
+            eyes.Open("Applitools site", "Screenshot test!");
+
+            // Load page image and validate.
+            Bitmap bitmap = GetImage(new Uri("https://applitools.com/tutorials/applitools.jpg"));
+
+            // Visual checkpoint.
+            eyes.CheckImage(bitmap);
+
+            // End the test.
+            eyes.Close();
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            eyes.AbortIfNotClosed();
+        }
+
+        static Bitmap GetImage(Uri uri)
+        {
+            var request = WebRequest.Create(uri);
+            using (var response = request.GetResponse())
+            using (var responseStream = response.GetResponseStream())
+            {
+                var imageStream = new MemoryStream();
+                responseStream.CopyTo(imageStream);
+                imageStream.Position = 0;
+
+                return new Bitmap(imageStream);
+            }
+        }
+    }
+}
